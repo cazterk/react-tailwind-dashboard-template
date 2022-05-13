1e2c49a332eb8f6b7afcb68adde401493b0e5b3e
Cephas Zulu <48860145+cazterk@users.noreply.github.com>
2022-05-13 20:02:41 +0200

    add header component

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,12 +1,21 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Header from "./header";
 import Sidebar from "./sidebar";
 
 const Layout = ({ children }: any) => {
   return (
     <>
       <div className="flex h-screen">
-        <Sidebar />
-        <main className="flex-auto bg-gray-100 p-10">{children}</main>
+        <div>
+          <Sidebar />
+        </div>
+
+        <div className="flex-auto bg-gray-100 ">
+          <div className="w-full h-20 fixed bg-gray-200">
+            <Header />
+          </div>
+          <main className=" p-24">{children}</main>
+        </div>
       </div>
     </>
   );
