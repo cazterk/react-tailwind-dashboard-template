e342d23321446fde2d3e8d817f4129090b3d8da9
cazterk <zcephas2@gmail.com>
2022-05-17 15:43:15 +0200

    add search bar

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Header = () => {
-  return <div>H</div>;
+  return (
+    <div className="flex  w-full  ">
+      <form action="" className=" flex justify-center w-full">
+        <input
+          type="text"
+          placeholder="Search"
+          autoComplete="off"
+          aria-label="Search"
+          className="px-3 py-2 border border-gray-300 rounded-md text-sm focus:outline-none focus:border-blue-500 focus:shadow-outline-blue w-4/12"
+        />
+      </form>
+    </div>
+  );
 };
 
 export default Header;
